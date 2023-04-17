01267c8d78664d88cd240249a2f39004d137f528
Aman Kumar Sah <readytogave@outlook.com>
2023-04-17 11:51:03 +0530

    This i sthe website init

---
+++ my-project/src/components/changes.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Changes() {
+  return (
+    <div>
+    <div className='text-3xl font-bold text-center mt-2 mb-1'>What is Eco_system</div>
+    <p className='text-center hover:text-base3 font-bold text-lg border-2 rounded mx-32'>An ecosystem is a system of living organisms and their physical environment that interact with each other</p>
+    <div className='inline  justify-center'>
+        <img className='inline-block mt-6 bg-cover align-baseline xl-block px-72' src="https://images.pexels.com/photos/1681452/pexels-photo-1681452.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+    </div>
+
+
+
+    </div>
+  )
+}
+
+export default Changes
---
+++ my-project/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors:{
+      "base1": "#87CBB9",
+      "base2": "#B9EDDD",
+      "tc-1":"#ffffff",
+      "base3": "#569DAA",
+    "tc-2": "#000000",}
+   
+    
+  },
+  plugins: [],
+}
+
